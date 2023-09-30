b84fc572d16582b9827be26fd646d803c89f65a1
Ignacio <igricart@gmail.com>
2023-09-30 17:52:34 +0200

    add gpt capabilities but still need some improvements

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include "Game.h"
+// TRY GPT
+#include "ChatGPT/include/ChatGPT.h"
+#include "ChatGPT/include/Error.h"
+#include <cstdlib>
 
 int main()
 {
@@ -7,6 +11,31 @@ int main()
 #else
   std::cout << "Debug build" << std::endl;
 #endif
+
+  // API token as argument
+  const char* apiKey = std::getenv("OPEN_API_KEY");
+  if (!apiKey)
+  {
+    std::cout << "Environment variable OPEN_API_KEY is not set." << std::endl;
+    return 1;
+  }
+  OpenAI::ChatGPT chatGpt{ apiKey };
+  try
+  {
+    // type of user and the message to ask
+    auto response = chatGpt.askChatGPT(
+        "user", "Give me a riddle and suggest two wrong answers and one correct, mark the correct one");
+    // Iterate all answers
+    for (const auto& choice : response.choices)
+    {
+      std::cout << choice.message.content;
+    }
+  }
+  catch (OpenAI::Error& e)
+  {
+    // JSON error returned by the server
+    std::cout << e.what();
+  }
 
   srand(time(NULL));
 
---
+++ third_party/ChatGPT/include/Error.h
@@ -0,0 +1,13 @@
+#include <string>
+#include <exception>
+namespace OpenAI
+{
+struct Error : public std::exception
+{
+  Error(std::string error);
+  const char* what() const noexcept override;
+
+private:
+  std::string message;
+};
+}  // namespace OpenAI
