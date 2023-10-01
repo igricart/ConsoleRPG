3e7bdc332670d8666794027e192fa0c5b6dcf4f2
Ignacio <igricart@gmail.com>
2023-10-01 23:22:53 +0200

    check GPT puzzle output

--- src/main.cpp
+++ src/main.cpp
@@ -12,31 +12,10 @@ int main()
   std::cout << "Debug build" << std::endl;
 #endif
 
-  // API token as argument
-  const char* apiKey = std::getenv("OPEN_API_KEY");
-  if (!apiKey)
+  for (int i = 0; i < 5; ++i)
   {
-    std::cout << "Environment variable OPEN_API_KEY is not set." << std::endl;
-    return 1;
+    GPTPuzzle::example();
   }
-  OpenAI::ChatGPT chatGpt{ apiKey };
-  try
-  {
-    // type of user and the message to ask
-    auto response = chatGpt.askChatGPT(
-        "user", "Give me a riddle and suggest two wrong answers and one correct, mark the correct one");
-    // Iterate all answers
-    for (const auto& choice : response.choices)
-    {
-      std::cout << choice.message.content;
-    }
-  }
-  catch (OpenAI::Error& e)
-  {
-    // JSON error returned by the server
-    std::cout << e.what();
-  }
-
   srand(time(NULL));
 
   Game game;
